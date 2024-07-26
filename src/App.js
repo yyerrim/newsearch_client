e1a7ecbb88ef5d785a68ec38a0d12224a41fcdeb
youngjun kim <youngjun4327@gmail.com>
2024-07-26 10:00:03 +0900

    merge conflict 임시해결

--- src/App.js
+++ src/App.js
@@ -1,82 +1,18 @@
 import { useState } from "react";
 import "./App.css";
 import ChatBot from "./components/ChatBot";
-import chatIcon from "./assets/chatProfile.png"; // 이미지 경로를 알맞게 수정하세요
+import chatIcon from "./assets/chatProfile.png";
 
-import Categories from './components/Categories';
-import Search from './components/Search';
-import MainNews from './components/MainNews';
-import SearchNews from './components/SearchNews';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-// function App() {
-//   const [category, setCategory] = useState('all');
-//   const [newsList, setNewsList] = useState([]);
-//   const [search, setSearch] = useState('');
-
-//   const fetchNews = async (search) => {
-//     setSearch(search);
-//     const url = `http://localhost:8080/naver/data?search=${search}`;
-//     const res = await fetch(url);
-//     const data = await res.json();
-//     setNewsList(data.items);
-//   };
-
-//   return (
-//     <BrowserRouter>
-//       <div className="App">
-//         <div className='menu'
-//           style={
-//             {
-//               display: 'flex',
-//               justifyContent: 'space-between'
-//             }
-//           }>
-//           <Categories onCategoryChange={setCategory} />
-//           <Search onSearch={fetchNews} />
-//         </div>
-//         <Routes>
-//           <Route path="/" element={<MainNews category={category} />} />
-//           <Route path="/search" element={<SearchNews newsList={newsList} search={search} />} />
-//         </Routes>
-//       </div>
-//     </BrowserRouter>
-//   );
-// }
+import Categories from "./components/Categories";
+import Search from "./components/Search";
+import MainNews from "./components/MainNews";
+import SearchNews from "./components/SearchNews";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [showChatBot, setShowChatBot] = useState(true);
-  const [messages, setMessages] = useState([
-    {
-      role: "assistant", // 초기 메시지도 assistant 역할로 변경
-      content: `안녕하세요? \n저는 gpt4o-mini 기반 챗봇입니다.\n뉴스를 보다가 궁금한 점을 물어보세요!\n(2023년 10월까지의 데이터 기반 답변입니다)`,
-    },
-  ]);
-
-  const handleButtonClick = () => {
-    setShowChatBot(!showChatBot);
-  };
-
-  return (
-    <div className="App">
-      {/* 버튼에 active 클래스 조건부 적용 */}
-      <button
-        className={`chatbot-button ${showChatBot ? "active" : ""}`}
-        onClick={handleButtonClick}
-      >
-        <img src={chatIcon} alt="Chat" className="chat-icon" />
-      </button>
-
-      {showChatBot && (
-        <div className="chatbot-overlay">
-          <ChatBot messages={messages} setMessages={setMessages} />
-        </div>
-      )}
-    </div>
-  const [category, setCategory] = useState('all');
+  const [category, setCategory] = useState("all");
   const [newsList, setNewsList] = useState([]);
-  const [search, setSearch] = useState('');
-
+  const [search, setSearch] = useState("");
   const fetchNews = async (search) => {
     setSearch(search);
     const url = `http://localhost:8080/naver/data?search=${search}`;
@@ -84,48 +20,84 @@ function App() {
     const data = await res.json();
     setNewsList(data.items);
   };
+
+  const [showChatBot, setShowChatBot] = useState(true);
+  const [messages, setMessages] = useState([
+    {
+      role: "assistant", // 초기 메시지도 assistant 역할로 변경
+      content: `안녕하세요? \n저는 gpt4o-mini 기반 챗봇입니다.\n뉴스를 보다가 궁금한 점을 물어보세요!\n(2023년 10월까지의 데이터 기반 답변입니다)`,
+    },
+  ]);
+  const handleButtonClick = () => {
+    setShowChatBot(!showChatBot);
+  };
+
   return (
     <BrowserRouter>
       <div className="App">
-        <div className='header' style={{ height: '15vh', backgroundColor: '#000060' }}></div>
-        <div className="container" style={{ height: '85vh', display: 'flex' }}>
-          <div className='news' style={{ width: '70vw', height: '100%' }}>
-            <div className='menu' style={
-              {
-                width: '100%', height: '10%', backgroundColor: 'lightseagreen',
-                display: 'flex', justifyContent: 'space-between', alignItems: 'center'
-              }
-            }>
+        <div
+          className="header"
+          style={{ height: "15vh", backgroundColor: "#000060" }}
+        ></div>
+        <div className="container" style={{ height: "85vh", display: "flex" }}>
+          <div className="news" style={{ width: "70vw", height: "100%" }}>
+            <div
+              className="menu"
+              style={{
+                width: "100%",
+                height: "10%",
+                backgroundColor: "lightseagreen",
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
               <Categories onCategoryChange={setCategory} />
               <Search onSearch={fetchNews} />
             </div>
-            <div className='article' style={{ width: '100%', height: '90%', backgroundColor: 'lightskyblue' }}>
+            <div
+              className="article"
+              style={{
+                width: "100%",
+                height: "90%",
+                backgroundColor: "lightskyblue",
+              }}
+            >
               <Routes>
                 <Route path="/" element={<MainNews category={category} />} />
-                <Route path="/search" element={<SearchNews newsList={newsList} search={search} />} />
+                <Route
+                  path="/search"
+                  element={<SearchNews newsList={newsList} search={search} />}
+                />
               </Routes>
             </div>
           </div>
-          <div className='chat' style={{ width: '30vw', height: '100%', backgroundColor: 'lightgreen' }}></div>
+          <div
+            className="chat"
+            style={{
+              width: "30vw",
+              height: "100%",
+              backgroundColor: "lightgreen",
+            }}
+          >
+            {/* 버튼에 active 클래스 조건부 적용 */}
+            <button
+              className={`chatbot-button ${showChatBot ? "active" : ""}`}
+              onClick={handleButtonClick}
+            >
+              <img src={chatIcon} alt="Chat" className="chat-icon" />
+            </button>
+
+            {showChatBot && (
+              <div className="chatbot-overlay">
+                <ChatBot messages={messages} setMessages={setMessages} />
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </BrowserRouter>
   );
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <div className='header' style={{ height: '20vh', backgroundColor: '#000060' }}></div>
-//       <div className="container" style={{ height: '80vh', display: 'flex' }}>
-//         <div className='news' style={{ width: '70vw', height: '100%' }}>
-//           <div className='menu' style={{ width: '100%', height: '10%', backgroundColor: 'lightseagreen' }}></div>
-//           <div className='article' style={{ width: '100%', height: '90%', backgroundColor: 'lightskyblue' }}></div>
-//         </div>
-//         <div className='chat' style={{ width: '30vw', height: '100%', backgroundColor: 'lightgreen' }}></div>
-//       </div>
-//     </div>
-//   );
-// }
 
 export default App;
--- src/components/MainNews.jsx
+++ src/components/MainNews.jsx
@@ -1,46 +1,44 @@
 import { useEffect, useState } from "react";
 
 const MainNews = ({ category }) => {
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-    useEffect(() => {
-        const getNews = async () => {
-            const cate = category === 'all' ? '' : `&category=${category}`;
-            const apiKey = process.env.REACT_APP_NEWS_API_KEY;
-            const url = `https://newsapi.org/v2/top-headlines?country=kr${cate}&apiKey=${apiKey}`;
-            const res = await fetch(url);
-            const datas = await res.json();
-            setData(datas.articles);
-        };
-        getNews();
-    }, [category]);
+  useEffect(() => {
+    const getNews = async () => {
+      const cate = category === "all" ? "" : `&category=${category}`;
+      const apiKey = process.env.REACT_APP_NEWS_API_KEY;
+      const url = `https://newsapi.org/v2/top-headlines?country=kr${cate}&apiKey=3edbe8fb44eb4142988957f180c08ef1`;
+      const res = await fetch(url);
+      const datas = await res.json();
+      setData(datas.articles);
+    };
+    getNews();
+  }, [category]);
 
-    return (
-        <div style={{ height: '100%', overflowY: 'scroll' }}>
-            {
-                data.map((v, i) => {
-                    return (
-                        <div key={i}>
-                            <a href={v.url}><h3>{v.title}</h3></a>
-                            <div className="content"
-                                style={{ display: 'flex' }}>
-                                {v.urlToImage &&
-                                    <img src={v.urlToImage} style={
-                                        {
-                                            width: '160px',
-                                            height: '100px',
-                                        }
-                                    } />
-                                }
-                                {v.description && <p>{v.description}</p>}
-                            </div>
-
-                        </div>
-                    )
-                })
-            }
-        </div >
-    );
+  return (
+    <div style={{ height: "100%", overflowY: "scroll" }}>
+      {data.map((v, i) => {
+        return (
+          <div key={i}>
+            <a href={v.url}>
+              <h3>{v.title}</h3>
+            </a>
+            <div className="content" style={{ display: "flex" }}>
+              {v.urlToImage && (
+                <img
+                  src={v.urlToImage}
+                  style={{
+                    width: "160px",
+                    height: "100px",
+                  }}
+                />
+              )}
+              {v.description && <p>{v.description}</p>}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
-
 export default MainNews;
