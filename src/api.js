1b3593c4404ffcba77ad5316a0697dfdee55b8db
youngjun kim <youngjun4327@gmail.com>
2024-07-29 13:16:22 +0900

    변경

--- src/api.js
+++ src/api.js
@@ -1,15 +1,52 @@
+// src/api.js
 const BACKEND_API_URL = process.env.REACT_APP_BACKEND_API_URL;
 
-export const fetchDataFromBackend = async (endpoint) => {
+export const fetchDataFromBackend = async (endpoint, options = {}) => {
   try {
-    const response = await fetch(`${BACKEND_API_URL}${endpoint}`);
+    console.log(`Fetching: ${BACKEND_API_URL}${endpoint}`);
+    const response = await fetch(`${BACKEND_API_URL}${endpoint}`, {
+      ...options,
+      headers: {
+        "Content-Type": "application/json",
+        ...options.headers,
+      },
+    });
+    console.log(`Response status: ${response.status}`);
+
     if (!response.ok) {
-      throw new Error("Network response was not ok");
+      throw new Error(
+        `Network response was not ok, status: ${response.status}`
+      );
     }
-    const data = await response.json();
-    return data;
+
+    const data = await response.json(); // JSON 파싱
+    return data; // 파싱된 데이터를 반환
   } catch (error) {
     console.error("There was a problem with the fetch operation:", error);
     throw error;
+  }
+};
+
+export const fetchNews = async (country = "kr") => {
+  const apiKey = "4d04ef5559d647efa5e26f934f7db879";
+  const url = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${apiKey}`;
+
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error("Error fetching news:", error);
   }
 };
